bb0d23a4c3a373897f0165775ae8f64111daab7a
Fernando Barros <fernando.barros@captalys.com.br>
2022-02-21 16:07:06 -0300

    Fixing tests because of Redux

--- src/__tests__/Header.js
+++ src/__tests__/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import { createSerializer } from '@emotion/jest';
 import { Header } from '../components/Header';
+import store from '../providers/store';
 
 expect.addSnapshotSerializer(createSerializer());
 
 describe('Header', () => {
     it('Should render fixed on top of the screen', async () => {
-        render(<Header />);
+        render(<Provider store={store}><Header /></Provider>);
 
-        const element = renderer.create(<Header />).toJSON();
+        const element = renderer.create(<Provider store={store}><Header /></Provider>).toJSON();
         const headerClass = element.props.className;
         const HeaderStyle = Array.from(document.getElementsByClassName(headerClass));
         const style = window.getComputedStyle(HeaderStyle[0]);
